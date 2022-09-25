5105fc0fb01e6f102c0298754c17d0a2cfd7bff5
Maega <maegastar@gmail.com>
2022-09-25 18:30:54 +1300

    Changed layout of box

--- frontend/src/pages/Moderator-Login.js
+++ frontend/src/pages/Moderator-Login.js
@@ -51,32 +51,33 @@ const Moderator_Login = () => {
 
   // JSX code for login form
   const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+    <div id="id01" class="modal">
+      <div className = "container">
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label>Username </label><br></br>
+              <input type="text" name="uname" required />
+              {renderErrorMessage("uname")}
+            </div>
+            <div className="input-container">
+              <label>Password </label><br></br>
+              <input type="password" name="pass" required />
+              {renderErrorMessage("pass")}
+            </div>
+            <div className="button-container">
+              <button type="submit">Login</button>
+            </div>
+          </form>
         </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="Login" />
-        </div>
-      </form>
+      </div>
     </div>
   );
 
   return (
-    <div className="app">
       <div className="login-form">
-        <div className="title">Sign In</div>
         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
       </div>
-    </div>
   );
 }
  
