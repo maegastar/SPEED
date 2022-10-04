320dcaf900d7a6ce76d93d54a89b8090d4174196
SandeshPathak <54426436+SandeshPathak@users.noreply.github.com>
2022-10-04 15:44:43 +0545

    Add 'Search Article' to navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,40 +7,43 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
+import SearchArticle from "./pages/Search-Article";
 import ModeratorLogin from "./pages/Moderator-Login";
 import AnalystLogin from "./pages/Analyst-Login";
 import NotFoundPage from "./pages/404";
 
-const App = () =>  {
-    return (
-      <Router>
-        <div>
-          
+const App = () => {
+  return (
+    <Router>
+      <div>
+
         <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-            <ul className="header">
-              <li><NavLink end to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-                <li><NavLink to = "/Moderator-Login">Moderator</NavLink></li>
-                <li><NavLink to = "/Analyst-Login">Analyst</NavLink></li>
-            </ul>
-            <div className="content">
-          
+        <ul className="header">
+          <li><NavLink end to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
+          <li><NavLink to="/SearchArticle">Search Articles</NavLink></li>
+          <li><NavLink to="/Moderator-Login">Moderator</NavLink></li>
+          <li><NavLink to="/Analyst-Login">Analyst</NavLink></li>
+        </ul>
+        <div className="content">
+
           <Routes>
-            <Route exact path="/" element={<Home/>}/>
-              <Route  path="/SEPractice" element={<SEPractice/>}/>
-              <Route  path="/SubmitArticle" element={<SubmitArticle/>}/>
-              <Route  path="/Moderator-Login" element={<ModeratorLogin/>}/>
-              <Route path="Analyst-Login" element={<AnalystLogin/>}/>
-              <Route  path="*" element={<NotFoundPage/>}/>
-              
+            <Route exact path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route path="/SearchArticle" element={<SearchArticle />} />
+            <Route path="/Moderator-Login" element={<ModeratorLogin />} />
+            <Route path="Analyst-Login" element={<AnalystLogin />} />
+            <Route path="*" element={<NotFoundPage />} />
+
           </Routes>
-          </div>
         </div>
-        </Router>
-        
-    );
+      </div>
+    </Router>
+
+  );
 }
- 
+
 export default App;
