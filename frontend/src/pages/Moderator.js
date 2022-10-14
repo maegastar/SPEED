e2bb1112db4bc9c556edd40115706ece6cca0fa5
Maega <maegastar@gmail.com>
2022-10-14 14:41:34 +1300

    2 tables set up with buttons and dropdowns

--- frontend/src/pages/Moderator.js
+++ frontend/src/pages/Moderator.js
@@ -5,11 +5,29 @@ import axios from 'axios';
 const Moderator = () => {
 
     const status = ""; //check for pending articles
+    const [pendingData, getPendingData] = useState([])
     const [data, getData] = useState([])
 
+
     useEffect(() => {
+        fetchPendingData()
         fetchData()
     }, [])
+ 
+    //add params for pending status
+    const fetchPendingData =() => { axios
+    .get('https://speed-website.herokuapp.com/api/SPEED/').then((response) => {
+        console.log(response.data);
+        getPendingData(response.data);
+    })
+    .catch(err => console.log("API error!"));
+    }
+
+    //handle status change
+    function onSelectChange(e){
+        console.log(e.target.value);
+        console.log(e.target.id)
+    }
  
     const fetchData =() => { axios
     .get('https://speed-website.herokuapp.com/api/SPEED/').then((response) => {
@@ -18,7 +36,7 @@ const Moderator = () => {
     })
     .catch(err => console.log("API error!"));
     }
-
+             
     const renderForm = (
         <div>
             <div className='pendingTable'>
@@ -31,7 +49,48 @@ const Moderator = () => {
                         <th>Description </th>
                         <th>Published Date </th>
                         <th>Publisher </th>
-                        
+                        <th>Status </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {pendingData.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.title}</td>
+                        <td>{item.author}</td>
+                        <td>{item.description}</td>
+                        <td>{item.published_date}</td>
+                        <td>{item.publisher}</td>
+                        <td><select id={i} onChange={onSelectChange.bind(this)}>
+                            <option value="current">currentStatus</option>
+                            <option value="pending">Pending</option>
+                            <option value="review">Review</option>
+                            <option value="reject">Reject</option>
+                        </select></td>
+                    </tr>
+                ))}
+                    </tbody>
+                </table>
+                </div>
+                <button>Submit</button>
+            </div>
+
+            <div className='allTable'>
+                <div className='buttons'>
+                <button>All </button>
+                <button>Accepted </button>
+                <button>Reviewing </button>
+                <button>Rejected </button>
+                </div>
+            <div className='container'>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Title </th>
+                        <th>Author </th>
+                        <th>Description </th>
+                        <th>Published Date </th>
+                        <th>Publisher </th>
+                        <th>Status</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -47,10 +106,6 @@ const Moderator = () => {
                     </tbody>
                 </table>
                 </div>
-
-            </div>
-
-            <div className='allTable'>
 
             </div>
 
