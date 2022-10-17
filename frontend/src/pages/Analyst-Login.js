a120cc86688944f8a50c7d8b94cc4149b31a86d3
Sandesh Pathak <Sandesh.pathak07@gmail.com>
2022-10-17 10:39:16 +0545

    Add navigation to Analyst dash, improve formatting

--- frontend/src/pages/Analyst-Login.js
+++ frontend/src/pages/Analyst-Login.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {
+  useNavigate
+} from "react-router-dom";
 
 const Analyst_Login = () => {
-      // React States
+  // React States
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     //Prevent page reload
@@ -19,17 +23,17 @@ const Analyst_Login = () => {
     axios
       .get('https://speed-website.herokuapp.com/api/SPEED/mod')
       .then(res => {
-       if(res.data[1].user === user){
-        if(res.data[1].pass === pass){
-          setIsSubmitted(true);
+        if (res.data[1].user === user) {
+          if (res.data[1].pass === pass) {
+            setIsSubmitted(true);
+          } else {
+            setIsSubmitted(false);
+            console.log("IS submitted is false");
+          }
         } else {
           setIsSubmitted(false);
-        console.log("IS submitted is false");
+          console.log("IS submitted is false");
         }
-       } else {
-        setIsSubmitted(false);
-        console.log("IS submitted is false");
-       }
       })
       .catch(err => {
         console.log("API Error!");
@@ -40,33 +44,33 @@ const Analyst_Login = () => {
   // JSX code for login form
   const renderForm = (
     <div>
-    <div id="id01" className="modal">
-      <div className = "container">
-        <div className="form">
-          <form onSubmit={handleSubmit}>
-            <div className="input-container moderator-analyst">
-              <label>Username </label><br></br>
-              <input type="text" name="user" id="user" required />
-            </div>
-            <div className="input-container moderator-analyst">
-              <label>Password </label><br></br>
-              <input type="password" name="pass" id="pass" required />
-            </div>
-            <div className="button-container">
-              <button type="submit">Login</button>
-            </div>
-          </form>
+      <div id="id01" className="modal">
+        <div className="container">
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <div className="input-container moderator-analyst">
+                <label>Username </label><br></br>
+                <input type="text" name="user" id="user" required />
+              </div>
+              <div className="input-container moderator-analyst">
+                <label>Password </label><br></br>
+                <input type="password" name="pass" id="pass" required />
+              </div>
+              <div className="button-container">
+                <button type="submit">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 
   return (
-      <div className="login-form">
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
+    <div className="login-form">
+      {isSubmitted ? navigate("/Analyst") : renderForm}
+    </div>
   );
 }
- 
+
 export default Analyst_Login;
